590f62f95856055846182d93fc4c47e4d08adb56
linwang929 <lin.wang@go.wustl.edu>
2017-11-24 23:39:55 -0600

    completed all three TODOs (with and without using "job-listing" class)

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static org.launchcode.models.JobData.findByColumnAndValue;
+import static org.launchcode.models.JobData.findByValue;
+
 /**
  * Created by LaunchCode
  */
@@ -23,5 +26,18 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> searchResults = new ArrayList<>();
 
+        if (searchType.equals("all")) {
+            searchResults = findByValue(searchTerm);
+        }
+        else {searchResults = findByColumnAndValue(searchType, searchTerm);}
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("searchResults", searchResults);
+
+        return "search";
+    }
 }
